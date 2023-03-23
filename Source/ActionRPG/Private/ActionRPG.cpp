d97afec206f8299d9d937c2f13ad6af74152c9ff
杨东 <yangdong0729@outlook.com>
2023-03-23 15:51:30 +0800

    initial commit

---
+++ Source/ActionRPG/Private/ActionRPG.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionRPG.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRPG, "UES_ActionRPG" );
+
+/** Logging definitions */
+DEFINE_LOG_CATEGORY(LogActionRPG);
+
+// 在 ActionRPG.h 中生命的 STAT 需要在源文件中定义
+DEFINE_STAT(STAT_HandleHealthChanged);
