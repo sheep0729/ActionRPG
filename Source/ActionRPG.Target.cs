e81398539393ee816a56191ffd2bf4fa2362ea07
Yang Dong <yangdong0729@outlook.com>
2023-09-11 11:35:50 +0800

    升级到 5.3

--- Source/ActionRPG.Target.cs
+++ Source/ActionRPG.Target.cs
@@ -9,8 +9,8 @@ public class ActionRPGTarget : TargetRules
 		: base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ActionRPG");
 	}
 }
--- Source/ActionRPGEditor.Target.cs
+++ Source/ActionRPGEditor.Target.cs
@@ -9,8 +9,8 @@ public class ActionRPGEditorTarget : TargetRules
 		: base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ActionRPG");
 	}
 }
